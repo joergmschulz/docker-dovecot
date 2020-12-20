416c6a485a4893baf05477b70b4490ffb00b5dfc
Jörg M. Schulz <js@v220200434720114782.hotsrv.de>
2020-12-20 23:31:33 +0100

    initial release

---
+++ imap/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+me=$(id -un)
+mygroup=$(id -gn)
+
+echo "inject ${me} as default user"
+sed -i "s/default_internal_user.*/default_internal_user = ${me}/" /etc/dovecot/dovecot.conf
+sed -i "s/default_login_user.*/default_login_user = ${me}/" /etc/dovecot/dovecot.conf
+sed -i "s/default_internal_group.*/default_internal_group = ${mygroup}/" /etc/dovecot/dovecot.conf
+sed -i "s/VMAIL_GROUP/${mygroup}/" /etc/dovecot/dovecot.conf
+sed -i "s/LDAP_PASSWORD/${LDAP_PASSWORD}/" /etc/dovecot/dovecot-ldap.conf.ext 
+sed -i "s/LDAP_HOSTS/${LDAP_HOSTS}/" /etc/dovecot/dovecot-ldap.conf.ext 
+
+# echo_info "ensure data path /data"
+# mkdir -p /data/dovecot/
+# mkdir -p /data/vmail/
+
+echo "start dovecot"
+# exec /bin/sh
+exec /usr/local/dovecot/sbin/dovecot -F 
