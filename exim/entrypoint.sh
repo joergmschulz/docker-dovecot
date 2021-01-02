89a68e3fee869c0650cbaed9ee12250e518daa60
Jörg Michael Schulz <info@joergschulz.de>
2021-01-02 22:55:13 +0100

    typos in entrypoint, added DOMAIN to env

--- exim/entrypoint.sh
+++ exim/entrypoint.sh
@@ -9,13 +9,13 @@ chown -R ${EXIM_USER}:${EXIM_GROUP} /var/log/exim
 
 
 cd /etc/exim/config.d
-sed -i "s/EXIM_EXTERNAL_HOSTNAME/mail.${domain}.de/" exim.conf
-sed -i "s/EXIM_LOCAL_DOMAINS/${domain}.de/" exim.conf
+sed -i "s/EXIM_EXTERNAL_HOSTNAME/mail.${DOMAIN}.de/" exim.conf
+sed -i "s/EXIM_LOCAL_DOMAINS/${DOMAIN}.de/" exim.conf
 
 # echo ${EXIM_PORTS} > EXIM_PORTS
 # echo ${SUBMISSION_PORT} > SUBMISSION_PORT
 # echo ${EXIM_TLS_ON_CONNECT_PORTS}>EXIM_TLS_ON_CONNECT_PORTS
 set -e
 # now we'd execute exim...
-/usr/exim/bin/exim -bd -q 15m -C /etc/exim/exim.conf
+/usr/exim/bin/exim -bd -q 15m -C /etc/exim/config.d/exim.conf
 # tail -f /var/log/exim/*g
