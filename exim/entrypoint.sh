0cd65c939de9c30b4a90cdc1ded1d7dcabb20623
Jörg Michael Schulz <info@joergschulz.de>
2021-01-03 23:17:19 +0100

    change order of SED commands

--- exim/entrypoint.sh
+++ exim/entrypoint.sh
@@ -10,13 +10,14 @@ chown -R ${EXIM_USER}:${EXIM_GROUP} /var/log/exim
 
 cd /etc/exim/config.d
 sed -i "s/EXIM_EXTERNAL_HOSTNAME/mail.${DOMAIN}.de/" exim.conf
+sed -i "s/LDAP_USER_FILTER/${LDAP_USER_FILTER}/" exim.conf
 sed -i "s/EXIM_LOCAL_DOMAINS/${DOMAIN}.de/" exim.conf
 sed -i "s/EXIM_LOCAL_DOMAINS/${EXIM_LOCAL_DOMAINS}/" exim.conf
 sed -i "s/LDAP_BASE/${LDAP_BASE}/" exim.conf
 sed -i "s/LDAP_PASSWORD/${LDAP_PASSWORD}/" exim.conf
 sed -i "s/LDAP_USER/${LDAP_USER}/" exim.conf
 sed -i "s/LDAP_HOSTS/${LDAP_HOSTS}/" exim.conf
-sed -i "s/LDAP_USER_FILTER/${LDAP_USER_FILTER}/" exim.conf
+
 
 
 # echo ${EXIM_PORTS} > EXIM_PORTS
