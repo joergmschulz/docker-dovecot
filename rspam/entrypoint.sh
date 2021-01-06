787ede8c8da2063460b9502b46c801894a6592ed
Jörg Michael Schulz <info@joergschulz.de>
2021-01-06 19:38:14 +0100

    include rspamd controller package

--- rspam/entrypoint.sh
+++ rspam/entrypoint.sh
@@ -5,11 +5,11 @@ cd /etc/rspamd/local.d
 sed -i "s/REDIS_PASSWORD/${REDIS_PASSWORD}/" redis.conf
 sed -i "s/RSPAM_REDIS_DB/${RSPAM_REDIS_DB}/" redis.conf
 
-printf "enable_password = \"${RSPAM_enable_password}\"\n\
-password = \"RSPAM_password\" = \"${RSPAM_password}\"\n" > worker-controller.inc
+printf "enable_password = \"${RSPAM_enable_password}\";\n\
+password = \"${RSPAM_password}\";\n\
+bind_socket = \"0.0.0.0:11334\";\n" > worker-controller.inc
 
 
-freshclam -u ${RSPAM_USER}
-clamd
-
-rspamd -f -u ${RSPAM_USER} -g $RSPAM_GROUP || exec "$@"
+( freshclam -u ${RSPAM_USER} && \
+clamd && \
+rspamd -f -u ${RSPAM_USER} -g $RSPAM_GROUP ) || exec "$@"
