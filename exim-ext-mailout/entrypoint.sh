7a26f5269ffdd9d4706fbf3e632a1adaa235231e
Jörg Michael Schulz <info@joergschulz.de>
2021-01-20 22:13:42 +0100

    correct some of the DKIM issues

--- exim-ext-mailout/entrypoint.sh
+++ exim-ext-mailout/entrypoint.sh
@@ -5,7 +5,8 @@ sed -i "s/EXIM_EXTERNAL_HOSTNAME/mailout.${DOMAIN}.de/" exim.conf
 sed -i "s/EXIM_RELAY_HOSTS/${EXIM_RELAY_HOSTS}/" exim.conf
 sed -i "s/EXIM_WHITELIST_NET/${EXIM_WHITELIST_NET}/" exim.conf
 sed -i "s/EXIM_QUALIFIED_DOMAIN/${DOMAIN}.de/" exim.conf
-sed -i "s/EXIM_DKIM_SELECTOR/${EXIM_DKIM_SELECTOR}.de/" exim.conf
+sed -i "s/EXIM_DKIM_SELECTOR/${EXIM_DKIM_SELECTOR}/" exim.conf
+sed -i "s/EXIM_DKIM_DOMAIN/${DOMAIN}.de/" exim.conf
 
 #  :
 # sed -i "s/IMAP_DOMAIN/${DOMAIN}/" exim.conf - not possible - lmtp only from internal network
