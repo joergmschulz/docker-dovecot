ff6eae2353d451babf2151e6c0e6d99c773d69b3
Jörg Michael Schulz <info@joergschulz.de>
2021-03-04 12:12:21 +0100

    upgrade dovecot to .14 version. Cleaned some comments.

--- exim-ext-mailout/entrypoint.sh
+++ exim-ext-mailout/entrypoint.sh
@@ -8,15 +8,7 @@ sed -i "s/EXIM_QUALIFIED_DOMAIN/${DOMAIN}.de/" exim.conf
 sed -i "s/EXIM_DKIM_SELECTOR/${EXIM_DKIM_SELECTOR}/" exim.conf
 sed -i "s/EXIM_DKIM_DOMAIN/${DOMAIN}.de/" exim.conf
 
-#  :
-# sed -i "s/IMAP_DOMAIN/${DOMAIN}/" exim.conf - not possible - lmtp only from internal network
 
-
-
-
-# echo ${EXIM_PORTS} > EXIM_PORTS
-# echo ${SUBMISSION_PORT} > SUBMISSION_PORT
-# echo ${EXIM_TLS_ON_CONNECT_PORTS}>EXIM_TLS_ON_CONNECT_PORTS
 set -e
-# now we'd execute exim...
+
 /usr/exim/bin/exim -bd -q 15m -C /etc/exim/config.d/exim.conf
