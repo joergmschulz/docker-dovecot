df6135427677228ec558fee8e6092cfef0bbbd38
Jörg Michael Schulz <info@joergschulz.de>
2022-12-03 23:20:27 +0100

    move neighbours info to options.inc

--- rspam/entrypoint.sh
+++ rspam/entrypoint.sh
@@ -14,9 +14,10 @@ bind_socket = \"0.0.0.0:11334\";\n" > worker-controller.inc
 
 sed -i "s/RSPAM_IP/${RSPAM_IP}/" worker-normal.inc
 
-printf "neighbours {\n\
+printf "\
+neighbours {\n\
     ${RSPAM_NEIGHBOURS}\n\
-}" > rspam-neighbours.inc
+}" >> options.inc
 
 # ( freshclam -u ${RSPAM_USER} && \
 # clamd && \
